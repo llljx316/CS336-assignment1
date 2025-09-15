5098501eca26a0348afd784b63ec79f596db5d72
lllljx0316 <1411545297@qq.com>
2025-09-15 11:21:10 +0800

    multithreading for pretokenization

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -1,12 +1,43 @@
 from . import PAT
 import regex as re
 import os
+import json
+from pathlib import Path
 from collections import defaultdict
 from .pretokenization_example import find_chunk_boundaries
 from tqdm import tqdm
+import multiprocessing as mp
 # import cProfile
 # import re
 # cProfile.run('re.compile("foo|bar")')
+
+# def pre_tokenize(text: str, pattern = PAT, special_tokens=None):
+    
+    
+def get_freq(corpus, freq):
+    for match in re.finditer(PAT, corpus):
+        word = tuple(bytes([b]) for b in match.group().encode("utf-8"))
+        freq[word] += 1
+
+def get_freq_without_special_tokens(chunk, freq, special_tokens):
+    # 先按 special token 切段，special token 本身作为单一词元 (bytes,) 计数
+    if len(special_tokens) == 0:
+        get_freq(chunk, freq)
+    else:
+        special_tokens_sorted = sorted(special_tokens, key=len, reverse=True)
+        special_tokens_re = re.compile("|".join(re.escape(st) for st in special_tokens_sorted)) if special_tokens_sorted else None
+        pos = 0
+        for sm in special_tokens_re.finditer(chunk):
+            if sm.start() > pos:
+                get_freq(chunk[pos:sm.start()], freq)
+            pos = sm.end()
+        if pos < len(chunk):
+            get_freq(chunk[pos:], freq)
+
+def process_chunk(chunk, special_tokens):
+    freq = defaultdict(int)
+    get_freq_without_special_tokens(chunk, freq, special_tokens)
+    return freq
 
 def train_bpe(
     input_path: str | os.PathLike,
@@ -36,46 +67,39 @@ def train_bpe(
                 Merges are ordered by order of creation.
     """
 
-    special_tokens_sorted = sorted(special_tokens, key=len, reverse=True)
-    special_tokens_re = re.compile("|".join(re.escape(st) for st in special_tokens_sorted)) if special_tokens_sorted else None
-
-    def get_freq(corpus, freq):
-        for match in re.finditer(PAT, corpus):
-            word = tuple(bytes([b]) for b in match.group().encode("utf-8"))
-            freq[word] += 1
     
-    def get_freq_without_special_tokens(chunk, freq):
-        # 先按 special token 切段，special token 本身作为单一词元 (bytes,) 计数
-        if len(special_tokens) == 0:
-            get_freq(chunk, freq)
-        else:
-            pos = 0
-            for sm in special_tokens_re.finditer(chunk):
-                if sm.start() > pos:
-                    get_freq(chunk[pos:sm.start()], freq)
-                pos = sm.end()
-            if pos < len(chunk):
-                get_freq(chunk[pos:], freq)
         
 
     def merge_two_bytes(bl):
         assert len(bl) == 2
         return bl[0]+bl[1]
+    
+
 
     with open(input_path, "rb") as f:
-        num_processes = 4
+        num_processes = mp.cpu_count()
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
         # The following is a serial implementation, but you can parallelize this
         # by sending each start/end pair to a set of processes.
         freq = defaultdict(int)
+        chunks = []
         for start, end in zip(boundaries[:-1], boundaries[1:]):
             f.seek(start)
             chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            # Run pre-tokenization on your chunk and store the counts for each pre-token
-            #special token split
-            get_freq_without_special_tokens(chunk, freq)
+            chunks.append(chunk)
+        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        #special token split
+        with mp.Pool(processes=num_processes) as pool:
+            # 读取每个数据块并并行处理
+            # chunks = [f.read(end - start).decode("utf-8", errors="ignore") for start, end in zip(boundaries[:-1], boundaries[1:])]
+            results = pool.starmap(process_chunk, [(chunk, special_tokens) for chunk in chunks])
 
+        # 合并所有进程的结果
+        freq = defaultdict(int)
+        for result in results:
+            for word, count in result.items():
+                freq[word] += count
         freq = dict(freq)
 
     vocab = {i:bytes([i]) for i in range(256)}
@@ -177,3 +201,57 @@ def train_bpe(
 
     return vocab, merges
 
+def save_data(save_dir, vocab, merges):
+    def bytes_to_unicode_escape(byte_value):
+        # return ''.join(f'\\u{byte:04x}' if byte > 127 else chr(byte) for byte in byte_value)
+        return ''.join(chr(byte) for byte in byte_value)
+
+        # try: 
+        #     return byte_value.decode('ascii')
+        # except:
+        #     ret = ''
+        #     for byte_v in byte_value:
+        #         ret += 
+        #     return f'\\u{byte_value[0]:04x}'
+        
+    root_dir = Path(save_dir)
+    with open(root_dir / "vocab.json", 'w', encoding='utf-8') as f:
+        json.dump({str(k): bytes_to_unicode_escape(v) for k, v in vocab.items()}, f)
+
+    with open(root_dir / "merges.txt", 'w', encoding='utf-8') as f:
+        # for a, b in merges:
+        f.writelines(f'"{bytes_to_unicode_escape(a)}" "{bytes_to_unicode_escape(b)}"\n' for a, b in merges)
+
+class tokenizer:
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens
+
+    @classmethod
+    def from_files(cls, vocab_filepath, merges_filepath, special_tokens=None):
+        with open(vocab_filepath, 'r', encoding='utf-8') as f:
+            vocab_data = json.load(f)
+            vocab = {int(k): bytes(v, 'utf-8') for k, v in vocab_data.items()}
+
+        def read_merges(file_path):
+            merges = []
+            with open(file_path, 'r', encoding='utf-8') as f:
+                for line in f:
+                    # 去掉行首尾的空白字符，并去掉引号
+                    parts = re.findall(r'"([^"]+)"', line)
+                    if len(parts) == 2:
+                        # 将两个部分转换为元组并添加到 merges 列表中
+                        merges.append((parts[0].strip('"').encode('utf-8'), parts[1].strip('"').encode('utf-8')))
+            return merges
+
+        # with open(merges_filepath, 'r', encoding='utf-8') as f:
+            # merges_data = [tuple(line.rstrip().split(" ")) for line in f]
+        merges = read_merges(merges_filepath)
+
+        return cls(vocab, merges, special_tokens)
+
+    def encode(self, text: str) -> list[int]:
+        pass
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tests.test_train_bpe import *
+from cs336_basics.bpe_tokenizer import *
 import time
 import cProfile
 import pstats
@@ -21,17 +22,26 @@ def test_train_bpe_special_tokens_t():
 
 
 if __name__=='__main__':
-    test_train_bpe()
+    # test_train_bpe()
     #train by tiny story
     # input_path = FIXTURES_PATH / "corpus.en"
     input_path = "../data/TinyStoriesV2-GPT4-valid.txt"
     start = time.time()
+
+
+
     vocab, merges = run_train_bpe(
         input_path=input_path,
         vocab_size=500,
         special_tokens=["<|endoftext|>"],
     )
     print(f'{time.time()-start}s.')
+    save_data('./tokenizer_data', vocab, merges)
+
+    # tokenizer.from_files('tokenizer_data/vocab.json','tokenizer_data/merges.txt', special_tokens=[b"<|endoftext|>"])
+
+
+
     # profiler = cProfile.Profile()
     # profiler.enable()  # 启动性能分析
     # vocab, merges = run_train_bpe(
